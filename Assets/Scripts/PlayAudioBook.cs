41b298e53c9a758921b9eb77e31ef26746f37e45
sosunnyproject <parksosun1103@gmail.com>
2021-07-01 23:50:41 +0900

    add new book scene

---
+++ Assets/Scripts/PlayAudioBook.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudioBook : MonoBehaviour
+{
+    public LightweightBookHelper lightweightBookHelper;
+
+    public AudioClip[] AudioList;
+    public AudioSource audioSource;
+
+    private int PageIndex = 0;
+    private int MaxPage = 3;
+    private float offset = 0.01f;
+    private bool playNext = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MaxPage = AudioList.Length;
+
+        audioSource.clip = AudioList[PageIndex];
+        audioSource.Play();
+        Debug.Log("audio length is: " + audioSource.clip.length);
+        Debug.Log(offset + audioSource.clip.length);
+        PageIndex++;
+        Debug.Log("next index is: " + PageIndex);
+        StartCoroutine(alertEnd());
+    }
+
+    private IEnumerator alertEnd()
+    {
+        Debug.Log("Start Waiting for" + audioSource.clip.length + offset);
+        yield return new WaitForSeconds(audioSource.clip.length + offset);
+        playNext = true;
+        Debug.Log("end of Audio + playNext?" + playNext);
+    }
+
+    private void Update()
+    {
+        if (playNext)
+        {
+            if (MaxPage > PageIndex)
+            {
+                Debug.Log("current index: " + PageIndex);
+                lightweightBookHelper.NextPage();
+                audioSource.clip = AudioList[PageIndex];
+                audioSource.Play();
+                Debug.Log("audio length is: " + audioSource.clip.length);
+                StartCoroutine(alertEnd());
+                PageIndex++;
+                Debug.Log("next index is: " + PageIndex);
+            }
+            playNext = false;
+        }
+    }
+}
